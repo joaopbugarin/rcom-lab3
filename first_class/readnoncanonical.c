9d0a6ccef0f7f576321d33550d61052b7b2f30ea
Bugarin <joaopbugarin@gmail.com>
2024-05-13 17:03:09 +0100

    update read file w/ state machine and some tests cases that still need changes

--- first_class/readnoncanonical.c
+++ first_class/readnoncanonical.c
@@ -9,25 +9,114 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
+#define FLAG 0x7E
+#define ESCAPE 0x7D
+#define A_RECEIVER_COMMAND 0x03
+#define C_SET 0x03
+
+ int STOP=FALSE;
+
+enum {
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_OK,
+    STOP_STATE,
+    DATA_RCV,
+    ESCAPE_RCV
+} state;
+
+char compute_bcc(char a, char c) {
+    return a ^ c;
+}
+
+void parse_frame(int fd, char c) {
+    static char buf[255];
+    static int i = 0;
+    switch (state) {
+        case START:
+            if (c == FLAG) {
+                state = FLAG_RCV;
+            }
+            break;
+        case FLAG_RCV:
+            if (c == A_RECEIVER_COMMAND) {
+                state = A_RCV;
+            } else if (c != FLAG) {
+                state = START;
+            }
+            break;
+        case A_RCV:
+            if (c == C_SET) {
+                state = C_RCV;
+            } else {
+                state = START;
+            }
+            break;
+        case C_RCV:
+            if (compute_bcc(A_RECEIVER_COMMAND, C_SET) == c) {
+                state = BCC_OK;
+            } else {
+                state = START;
+            }
+            break;
+        case BCC_OK:
+            if (c == FLAG) {
+                state = STOP_STATE;
+            } else if (c == ESCAPE) {
+                state = ESCAPE_RCV;
+            } else {
+                buf[i++] = c;
+                state = DATA_RCV;
+            }
+            break;
+        case DATA_RCV:
+            if (c == FLAG) {
+                // End of frame, process data
+                write(fd, buf, i);
+                i = 0;
+                state = START;
+            } else if (c == ESCAPE) {
+                state = ESCAPE_RCV;
+            } else {
+                buf[i++] = c;
+            }
+            break;
+        case ESCAPE_RCV:
+            buf[i++] = c ^ 0x20;
+            state = DATA_RCV;
+            break;
+        case STOP_STATE:
+            state = START;
+            break;
+    }
+}
 
 int main(int argc, char** argv)
 {
     int fd,c, res;
     struct termios oldtio,newtio;
-    unsigned char buf[5]; // Change the buffer size to 5
+    char buf[255];
 
     if ( (argc < 2) ||
-         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+         ((strcmp("/dev/ttyS10", argv[1])!=0) &&
+          (strcmp("/dev/ttyS11", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
 
+
+    /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+    */
+
+
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd < 0) { perror(argv[1]); exit(-1); }
 
-    if (tcgetattr(fd,&oldtio) == -1) {
+    if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
         perror("tcgetattr");
         exit(-1);
     }
@@ -37,10 +126,17 @@ int main(int argc, char** argv)
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
 
+    /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;
-    newtio.c_cc[VMIN]     = 5;
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+    /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) próximo(s) caracter(es)
+    */
+
 
     tcflush(fd, TCIOFLUSH);
 
@@ -51,13 +147,18 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
-    while (STOP==FALSE) {
-        res = read(fd,buf,5);   /* returns after 5 chars have been input */
-        if (buf[0] == 0x5c && buf[1] == 0x03 && buf[2] == 0x08 && buf[3] == (buf[1] ^ buf[2]) && buf[4] == 0x5c) {
-            STOP = TRUE;
-            printf("Received valid supervision frame\n");
+
+    while (STOP==FALSE) {       /* loop for input */
+        res = read(fd,buf,255);   /* returns after 5 chars have been input */
+        buf[res]=0;               /* so we can printf... */
+        printf(":%s:%d\n", buf, res);
+        if (buf[0]=='z') STOP=TRUE;
+
+        for (int i = 0; i < res; i++) {
+            parse_frame(fd, buf[i]);
         }
     }
+
 
     tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
